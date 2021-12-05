e5bd5951c53e52cc3ba95f1856ab116b6641df63
Gabriel Vilches <gabriel.vilches@xero.com>
2021-12-05 16:05:59 +1300

    wip 12.6 done

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,18 +1,17 @@
 use std::env;
 use std::process;
 
-mod lib;
-
-use lib::Config;
-use lib::run;
+mod minigrep;
 
 fn main() {
     let args = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|_err| {
+    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    if let Err(_e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
         process::exit(1);
     };
 }
