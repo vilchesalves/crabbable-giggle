b0ac4e70b9e2cdcccead9e6eb05263b99b69444a
Gabriel Vilches <gabriel.vilches@xero.com>
2022-01-30 09:01:57 +1300

    done 15.2

--- rust/src/box.rs
+++ rust/src/box.rs
@@ -3,7 +3,7 @@
 #[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
-    Nil
+    Nil,
 }
 
 pub fn main() {
@@ -15,11 +15,43 @@ pub fn main() {
 
 #[cfg(test)]
 mod tests {
-
     #[test]
     fn main() {
         super::main();
 
         assert_eq!(0, 0);
+    }
+
+    #[test]
+    fn dereference() {
+        struct MyBox<T>(T);
+
+        impl<T> MyBox<T> {
+            fn new(value: T) -> MyBox<T> {
+                MyBox(value)
+            }
+        }
+
+        impl<T> std::ops::Deref for MyBox<T> {
+            type Target = T;
+
+            fn deref(&self) -> &Self::Target {
+                &self.0
+            }
+        }
+
+        fn hello(name: &str) {
+            println!("Hello, {name}!");
+        }
+
+        let x = 5;
+        let y = MyBox::new(x);
+
+        assert_eq!(5, x);
+        assert_eq!(5, *y);
+
+        let string_box = MyBox::new(String::from("I'm a string in a box."));
+
+        hello(&string_box);
     }
 }
