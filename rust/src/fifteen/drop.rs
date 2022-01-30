4a8b76e383dbe45b867b3453bcedef3708c2f3ce
Gabriel Vilches <gabriel.vilches@xero.com>
2022-01-31 12:42:37 +1300

    15.3 done

--- rust/src/fifteen/drop.rs
+++ rust/src/fifteen/drop.rs
@@ -1,8 +1,30 @@
+#![allow(dead_code)]
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        let data = &self.data;
+        println!("I'm dropping the pointer, it had data {data}");
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn main() {
+        let _c = CustomSmartPointer {
+            data: String::from("Point C"),
+        };
+        let _d = CustomSmartPointer {
+            data: String::from("Point D"),
+        };
 
+        println!("Pointers created.");
+        drop(_c);
+        println!("end of scope");
     }
 }
