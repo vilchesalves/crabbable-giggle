2f8683f9ef643d24ff8281ae1052d7db40faa4d0
Gabriel Vilches <gabriel.vilches@xero.com>
2022-03-13 16:54:00 +1300

    15.5.3 done

--- rust/src/fifteen/rc.rs
+++ rust/src/fifteen/rc.rs
@@ -26,3 +26,28 @@ fn rc() {
         Rc::strong_count(&a)
     );
 }
+
+#[test]
+fn mutable() {
+    use std::cell::RefCell;
+
+    #[derive(Debug)]
+    enum Lista {
+        Cons(Rc<RefCell<i32>>, Rc<Lista>),
+        Nil,
+    }
+
+    use Lista::{Cons, Nil};
+
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+}
--- rust/src/fifteen/refcell.rs
+++ rust/src/fifteen/refcell.rs
@@ -29,15 +29,52 @@ where
         if percent_of_max > 1.0 {
             self.messenger.send("Error: You are over your quota!");
         } else if percent_of_max >= 0.8 {
-            self.messenger.send("Urgent warning: You've used up over 80% of your quota!");
-        } else if percent_of_max >= 0.66{
-            self.messenger.send("Warning: You've used up over 66% of your quota!");
+            self.messenger
+                .send("Urgent warning: You've used up over 80% of your quota!");
+        } else if percent_of_max >= 0.66 {
+            self.messenger
+                .send("Warning: You've used up over 66% of your quota!");
         }
     }
 }
 
 #[cfg(test)]
 mod test {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(message.to_string());
+        }
+    }
+
     #[test]
-    fn main() {}
+    fn it_sends_a_quota_warning() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        let message_contains_quota = mock_messenger
+            .sent_messages
+            .borrow()
+            .get(0)
+            .unwrap_or(&String::from(""))
+            .contains("quota");
+
+        assert!(message_contains_quota);
+    }
 }
